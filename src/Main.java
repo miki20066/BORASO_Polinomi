705fd55ea5b8633f1ba84981cacfff40d40bfad3
michelangelo <9698035@itisrossi.vi.it>
2025-10-05 18:27:39 +0200

    aggiunto un main per provare le funzioni

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
 
+                retta r = new retta(3.0, -6.0);
+                r.soluzioni();
+
+                System.out.println("Coefficienti: " + r.stampaCoeficenti());
+                System.out.println("Soluzione: " + r.stampaSoluzioni());
+
+                parabola p = new parabola(1.0, -5.0, 6.0);
+                p.soluzioni();
+
+                System.out.println("Coefficienti: " + p.stampaCoeficenti());
+                System.out.println("Soluzioni: " + p.stampaSoluzioni());
 
 
-
-    }
-}
+            }
+        }
